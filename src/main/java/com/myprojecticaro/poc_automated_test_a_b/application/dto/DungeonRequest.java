9fc00a1a2c2102a0e71502b17da23c6b2bf7a28a
icaetanodefigueiredo <icaetanodefigueiredo@lendingclub.com>
2025-08-29 11:25:37 -0700

    feat: update body

--- src/main/java/com/myprojecticaro/poc_automated_test_a_b/application/dto/DungeonRequest.java
+++ src/main/java/com/myprojecticaro/poc_automated_test_a_b/application/dto/DungeonRequest.java
@@ -1,5 +1,11 @@
 package com.myprojecticaro.poc_automated_test_a_b.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
-public record DungeonRequest(@NotNull int[][] dungeon) {}
+public record DungeonRequest(
+        @Schema(
+                description = "Dungeon grid for the experiment",
+                example = "[[0,-2,-3],[-1,-2,-1],[10,30,-5]]"
+        )
+        @NotNull int[][] dungeon) {}
